fc93bca30bbe5360c6a1316894294a955a36e7a2
vikas <vikaspatil212222@gmail.com>
2023-08-19 22:12:03 +0530

    socket io

--- src/Components/ChatArea/ChatArea.tsx
+++ src/Components/ChatArea/ChatArea.tsx
@@ -5,7 +5,9 @@ import io from "socket.io-client";
 export const ChatArea: FC = () => {
     const [users, setUsers] = useState([]);
     const socket = io("https://socket-io-server-gilt.vercel.app/",{
+        path: '/socket.io',
         transports: ['websocket'],
+        secure: true,
 
     });
     useEffect(() => {
